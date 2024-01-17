72abfe2ba1a308d79d9e5c66b88e1965267e64ad
kitakou0313 <kitakou0313@gmail.com>
2024-01-17 22:02:37 +0900

    add: add option to change min iter num

--- rsync_files_with_tqdm/main.py
+++ rsync_files_with_tqdm/main.py
@@ -7,7 +7,7 @@ def rsync_copy(source, destination):
     command = ["rsync", "-a", source, destination]
     subprocess.run(command, check=True)
 
-def copy_files(source, destinations:list):
+def copy_files(source, destinations:list, min_iter_num):
     all_items = []
     for root, dirs, files in os.walk(source):
         for item in dirs + files:
@@ -15,13 +15,11 @@ def copy_files(source, destinations:list):
             relative_path = os.path.relpath(item_path, source)
             all_items.append((item_path, relative_path))
 
-    for source_path, relative_path in all_items:
-        for destination in destinations:
-            print("{} is copied to {}".format(source_path, os.path.join(destination, relative_path)))
+    
+    print("{} files in {} is copied to {}".format(len(all_items), source, destinations))
 
-    miniters = len(all_items) // 10000
     # Use tqdm to display a progress bar for each file and each destination
-    for source_path, relative_path in tqdm(all_items, desc="Copying files", unit="item", miniters=miniters):
+    for source_path, relative_path in tqdm(all_items, desc="Copying files", unit="item", miniters=min_iter_num):
         for destination in destinations:
             dest_path = os.path.join(destination, relative_path)
             os.makedirs(os.path.dirname(dest_path), exist_ok=True)
@@ -30,12 +28,13 @@ def copy_files(source, destinations:list):
 def main():
     parser = argparse.ArgumentParser(description="Copy files and directories with progress bar using rsync to multiple destinations")
     parser.add_argument("source", help="Source directory path")
+    parser.add_argument("min_iter_num", type=int, help="min iter num")
     parser.add_argument("destinations", nargs="+", help="List of destination directory paths")
     args = parser.parse_args()
 
     print(args)
 
-    copy_files(args.source, args.destinations)
+    copy_files(args.source, args.destinations, args.min_iter_num)
 
 if __name__ == "__main__":
     main()
--- rsync_files_with_tqdm/main.sh
+++ rsync_files_with_tqdm/main.sh
@@ -2,6 +2,8 @@
 
 source_path="$1"
 shift
+min_iter_num="$1"
+shift
 dest_paths="$@"
 
-docker compose run -d --rm rsync bash -c "python main.py $source_path ${dest_paths[@]} &> log.txt"
+docker compose run -d --rm rsync bash -c "python main.py ${source_path} ${min_iter_num} ${dest_paths[@]} &> log.txt"
