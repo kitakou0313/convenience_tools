da82f439b49bf2098cd29d1326f17db9c2001d4a
kitakou0313 <kitakou0313@gmail.com>
2024-02-18 23:33:02 +0900

    add: change args to change log filename

--- rsync_files_with_tqdm/main.sh
+++ rsync_files_with_tqdm/main.sh
@@ -4,6 +4,8 @@ source_path="$1"
 shift
 min_iter_num="$1"
 shift
+log_file_name="$1"
+shift
 dest_paths="$@"
 
-docker compose run -d --rm rsync bash -c "python main.py ${source_path} ${min_iter_num} ${dest_paths[@]} &> log.txt"
+docker compose run -d --rm rsync bash -c "python main.py ${source_path} ${min_iter_num} ${dest_paths[@]} &> log_${log_file_name}.txt"
