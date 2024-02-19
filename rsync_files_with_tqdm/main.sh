ac8c56da3e104cfe1207ff1de72d6aa2870aa144
kitakou0313 <kitakou0313@gmail.com>
2024-02-19 12:15:58 +0900

    add: add option to change yaml

--- rsync_files_with_tqdm/main.sh
+++ rsync_files_with_tqdm/main.sh
@@ -6,6 +6,8 @@ min_iter_num="$1"
 shift
 log_file_name="$1"
 shift
+format_file="$1"
+shift
 dest_paths="$@"
 
-docker compose run -d --rm rsync bash -c "python main.py ${source_path} ${min_iter_num} ${dest_paths[@]} &> log_${log_file_name}.txt"
+docker compose -f "${format_file}" run -d --rm rsync bash -c "python main.py ${source_path} ${min_iter_num} ${dest_paths[@]} &> log_${log_file_name}.txt"
